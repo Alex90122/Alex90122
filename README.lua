55905410f7f2cc6a170e451d449fac0f7b1f37aa
Alex90122 <158450645+Alex90122@users.noreply.github.com>
2024-02-23 16:45:48 +0300

    README.lua
    The things about me there

--- README.lua
+++ README.lua
@@ -1,6 +1,6 @@
 - Hi I'm Alex90122
 - if you seeing this profile I'm new to github
 - I'm from turkey and I speak turkish and english
-  if you abusing linkvertise for get some scripts for hack u can abuse this site for bypass linkvertise: bypass.city
+  if you abusing linkvertise for get some scripts for hack u can abuse this site for bypass linkvertise: bypass.city/
 - {see ya}
     ^_^
